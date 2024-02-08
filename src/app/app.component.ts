b5e3c9d75e0a7c8a4aa3e22aa85e1f171543d686
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-08 15:23:49 -0600

    routes wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,FooterComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
