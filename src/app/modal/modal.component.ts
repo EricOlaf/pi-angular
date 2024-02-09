837abb0a553cbd5829bc27fa4dca79830da35384
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-09 16:12:18 -0600

    modal

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+  @Input() isOpen: boolean = false
+  @Output() closeModal = new EventEmitter<void>()
+  onClose(){
+    this.closeModal.emit()
+  }
+}
+
