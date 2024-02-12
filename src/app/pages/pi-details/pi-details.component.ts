1734672069bb6a7b00a23b8fcea692bb41b3f893
EricOlaf <olavesoneric@gmail.com>
2024-02-12 10:13:17 -0700

    Routing for the basic pages

---
+++ src/app/pages/pi-details/pi-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pi-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './pi-details.component.html',
+  styleUrl: './pi-details.component.css'
+})
+export class PiDetailsComponent {
+
+}
