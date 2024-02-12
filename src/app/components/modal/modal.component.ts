d216544529a2a29ce0174f1a176be2ab15a09721
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-12 12:20:07 -0600

    modal close button

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faCircleXmark} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
+  faCircleXmark= faCircleXmark
   @Input() isOpen: boolean = false
   @Output() closeModal = new EventEmitter<void>()
   onClose(){
