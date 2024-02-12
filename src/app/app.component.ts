200b0334c122301cdc769c3e41350b59fa6f0294
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-12 12:06:54 -0600

    updated css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
