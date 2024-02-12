0982d24120555ca26e7908a370872a3ad5b7653f
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-12 15:36:48 -0600

    contact form

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,20 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import {faCircleXmark} from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule,FontAwesomeModule],
+  imports: [CommonModule,FontAwesomeModule,  ReactiveFormsModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  
   faCircleXmark= faCircleXmark
+  
+  contactForm : FormGroup
+
   @Input() isOpen: boolean = false
+  
   @Output() closeModal = new EventEmitter<void>()
+
+  constructor(private formBuilder: FormBuilder ) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+     })
+  }
+
+  ngOnInit(): void {}
+
+  sendContactInfo():void {
+  console.log("CONTACT FORM SUBMITTED",this.contactForm)
+  }
+
   onClose(){
     this.closeModal.emit()
   }
 }
+
 
