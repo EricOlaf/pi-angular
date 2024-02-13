62719be711ee1661e042e230aa989b63e53146e2
IanHancock36 <71861289+IanHancock36@users.noreply.github.com>
2024-02-13 14:28:40 -0600

    Contact modal  (#5)
    * modal
    
    * moved modal and footer to components folder
    
    * updated css
    
    * modal close button
    
    * contact form
    
    * contact form mild validations
    
    * contact form reset
    
    * form
    
    * styling
    
    * contact form
    
    ---------
    
    Co-authored-by: Ian Hancock <ianhancock@Ians-MacBook-Pro.local>

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ModalComponent } from '../modal/modal.component';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ModalComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  isModalOpen: boolean = false
+
+  openModal(){
+    this.isModalOpen = true
+  }
+  onModalClose() {
+    this.isModalOpen = false;
+  }
 
 }
---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,58 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule,FontAwesomeModule,ReactiveFormsModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+
+export class ModalComponent implements OnInit {
+
+  faCircleXmark= faCircleXmark
+  contactForm: FormGroup
+  @Input() isSubmitted: boolean = false 
+  @Input() isOpen: boolean = false
+  @Output() closeModal = new EventEmitter<void>()
+
+  constructor(private formBuilder: FormBuilder, private routes: Router) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+     })
+  }
+
+  ngOnInit():void {}
+
+  sendContactInfo():void {
+    if (this.contactForm.status === "VALID"){
+      this.isSubmitted = true
+      console.log("CONTACT FORM SUBMITTED",this.contactForm)
+    } else {
+      console.log("CONTACT FORM ERROR please try again")
+    }
+  }
+
+  onClose(){
+    this.closeModal.emit()
+    this.contactForm.reset();
+    this.routes.navigate(['/']);
+  }
+
+  resetForm() {
+    this.isSubmitted = false;
+    this.contactForm.reset();
+    this.closeModal.emit()
+    this.routes.navigate(['/']);
+  }
+}
+
+
