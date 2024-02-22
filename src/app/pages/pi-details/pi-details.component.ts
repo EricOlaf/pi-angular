b33f9cff4cc37038b2258ad1f60af9d9e0a74d87
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-22 13:41:47 -0600

    unsubscribe

--- src/app/pages/pi-details/pi-details.component.ts
+++ src/app/pages/pi-details/pi-details.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { PiDetailsService } from '../../pi-details.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pi-details',
@@ -11,28 +12,9 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './pi-details.component.html',
   styleUrl: './pi-details.component.css',
 })
-export class PiDetailsComponent implements OnInit {
+export class PiDetailsComponent implements OnInit, OnDestroy {
+  
   piProfileId: any;
-
-  constructor(
-    private route: ActivatedRoute,
-    private piProfileData: PiDetailsService
-  ) {}
-
-  ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      this.piProfileId = params.get('id');
-      this.fetchPiProfileId();
-    });
-  }
-  fetchPiProfileId(): void {
-    this.piProfileData
-      .getProfileDetailsData(this.piProfileId)
-      .subscribe((data) => {
-        this.piProfileData = data;
-      });
-  }
-
   title: string = 'TITLE';
   name: string = 'Samantha Reed';
   rating: string = '88';
@@ -43,4 +25,30 @@ export class PiDetailsComponent implements OnInit {
     'Samantha Reed is an expert in corporate espionage and fraud investigation. She has a sharp mind and a vast network of informants, making her the go-to PI for businesses facing internal threats.';
   reviews: string =
     'Samantha was the best PI I could ever hope for blah blah blah blah';
+
+  constructor(
+    private route: ActivatedRoute,
+    private piProfileData: PiDetailsService,
+    private subscription: Subscription
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription = this.route.paramMap.subscribe((params) => {
+      this.piProfileId = params.get('id');
+      this.fetchPiProfileId();
+    });
+  }
+
+  fetchPiProfileId(): void {
+    this.piProfileData
+      .getProfileDetailsData(this.piProfileId)
+      .subscribe((data) => {
+        this.piProfileData = data;
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 }
